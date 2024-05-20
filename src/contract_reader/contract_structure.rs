2afae1cdddf065319f3851e8db62c62146dfdf97
walefy <walefyd1@gmail.com>
2024-05-20 17:48:55 -0300

    feat: add new data structures for contract handling

---
+++ src/contract_reader/contract_structure.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(untagged)]
+pub enum BodyType {
+    Number(i32),
+    String(String),
+    HashMap(HashMap<String, BodyType>),
+    Array(Vec<BodyType>),
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Expect {
+    pub status: u16,
+    pub body: BodyType,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Method {
+    pub method_type: String,
+    pub body: BodyType,
+    pub expect: Expect,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Contract {
+    pub base_url: String,
+    pub timeout: u16,
+    pub methods: Vec<Method>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+mod contract_reader;
+
+use contract_reader::contract_structure::Contract;
+use serde_json::Result;
+
+fn main() -> Result<()> {
+    let example = r#"
+        {
+            "base_url": "http://localhost:8000/",
+            "timeout": 10000,
+            "methods": [
+                {
+                    "method_type": "GET",
+                    "body": { "name": "walefy" },
+                    "expect": {
+                        "status": 500,
+                        "body": "feito"
+                    }
+                }
+            ]
+        }
+    "#;
+
+    let contract_example: Contract = serde_json::from_str(&example)?;
+
+    println!("{:#?}", contract_example.methods[0].body);
+    Ok(())
 }
