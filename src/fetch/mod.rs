95554a22efefa0ca377f7b088ab34c48a32da818
walefy <walefyd1@gmail.com>
2024-05-21 14:18:53 -0300

    refactor: remove methods module

--- src/fetch/mod.rs
+++ src/fetch/mod.rs
@@ -1,12 +1,12 @@
-mod methods;
 mod response;
 
 use reqwest::blocking::Client;
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use std::collections::HashMap;
+use std::time::Duration;
 
 use crate::contract_reader::contract_structure::{BodyType, HttpMethod};
 
-use self::methods::build_request;
 use self::response::Response;
 
 pub fn fetch(
@@ -17,5 +17,31 @@ pub fn fetch(
     timeout: &u64,
 ) -> Response {
     let client = Client::new();
-    build_request(method, &url, headers, body, timeout, &client)
+
+    let body_str = body.to_string().unwrap_or("null".to_string());
+    let mut l_headers: HeaderMap = HeaderMap::new();
+
+    for (k, v) in headers.iter() {
+        l_headers.insert(
+            HeaderName::from_bytes(k.as_bytes()).unwrap(),
+            HeaderValue::from_bytes(v.as_bytes()).unwrap(),
+        );
+    }
+
+    let request = match method {
+        HttpMethod::Get => client.get(url),
+        HttpMethod::Post => client.post(url),
+    };
+
+    let res = request
+        .body(body_str)
+        .headers(l_headers)
+        .timeout(Duration::from_millis(timeout.to_owned()))
+        .send()
+        .unwrap();
+
+    Response {
+        status: res.status().as_u16(),
+        body: serde_json::from_str(&res.text().unwrap()).unwrap(),
+    }
 }
